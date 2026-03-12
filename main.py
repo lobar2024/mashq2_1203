1d3447e2a29a0303339ed8c6d064d62e763524a6
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-03-12 20:50:19 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#2
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def introduce(self):
+#         print(f"Mening ismim: {self.name}")
+#
+#
+# class Student(Person):
+#     def __init__(self, name, age, faculty):
+#         super().__init__(name, age)
+#         self.faculty = faculty
+#
+#     def introduce(self):
+#         super().introduce()
+#         print(f"Men {self.faculty} fakultetida o'qiyman")
+#
+#
+# s = Student('Ali', 20, 'Yuridistika')
+# s.introduce()
